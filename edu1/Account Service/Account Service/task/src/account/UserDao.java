35e8f199f89e3a530947571876b82c674423e35a
holdmelol <ineedabraintoo@gmail.com>
2022-04-21 21:56:00 +0300

    newQuery

--- edu1/Account Service/Account Service/task/src/account/UserDao.java
+++ edu1/Account Service/Account Service/task/src/account/UserDao.java
@@ -3,14 +3,24 @@ package account;
 import java.sql.*;
 
 public class UserDao extends Exception {
-    static final String JDBC_DRIVER = "org.h2.Driver";
     static final String DB_URL = "jdbc:h2:file:../service_db";
 
     static final String USER = "sa";
     static final String PASS = "abc123";
 
-    Connection connection;
-    Statement statement;
+    String QUERY = "SELECT userId, name, lastname, email FROM user";;
+
+
+
+    public void getUser() {
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(QUERY)) {
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
